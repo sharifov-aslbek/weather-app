35b0af391cdc7092de22fa641516cc9cc7df1b37
Sharifov Aslbek <148238968+sharifov-aslbek@users.noreply.github.com>
2024-05-30 11:44:56 +0500

    fixing errors

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ search.addEventListener("click" , () => {
          }
 
          temperature.innerHTML = `${parseInt(json.main.temp)}<span>C</span>`;
-         // description.innerHTML =  `${json.weather[0].description}`;
+         description.innerHTML =  `${json.weather[0].description}`;
          humidity.innerHTML = `${json.main.humidity}%`
          wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
 
